6e0d01ecbd7a5fe9bae86f359442b31bc1da8a4b
Aparna-015 <aparnaraman995@gmail.com>
2026-08-07 20:19:11 +0530

    INITIAL COMMIT

---
+++ transaction-app/src/App.jsx
@@ -0,0 +1,16 @@
+import Transaction from "./component/Transaction"
+
+
+function App() {
+
+  return (
+   
+      <div>
+        <Transaction/>
+        <h3>gehh</h3>
+      </div>
+   
+  )
+}
+
+export default App
---
+++ transaction-app/src/component/Transaction.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+const Initial_transactions = [
+  {
+    id: 1,
+    title: "Cash",
+    amount: 500,
+    status: "pending",
+  },
+  {
+    id: 2,
+    title: "NN",
+    amount: 500,
+    status: "completed",
+  },
+  {
+    id: 3,
+    title: "OO",
+    amount: 500,
+    status: "pending",
+  },
+  {
+    id: 4,
+    title: "Cash",
+    amount: 500,
+    status: "completed",
+  },
+];
+
+const Transaction = () => {
+  const [state, setState] = useState(Initial_transactions);
+  console.log(state, "state=====");
+  const [data, setData] = useState("completed");
+  console.log(data, "data----");
+
+  const displayedTransactions = state.filter((tx) => {
+    if (data === "pending") return tx.status === "pending";
+    if (data === "completed") return tx.status === "completed";
+    return true; // 'all'
+  });
+  console.log(displayedTransactions, "tra");
+
+  return (
+    <div>
+      <h1>ghhh</h1>
+      show transactions
+      <button onClick={() => setData("pending")}>pending</button>
+      <button onClick={() => setData("completed")}>completed</button>
+      <button onClick={() => setData("all")}>all</button>
+      <div>
+        {displayedTransactions.map((item) => (
+          <div key={item.id}>
+            
+            <h3>{item.title}</h3>
+            <h1>{item.title}</h1>
+            <p>Amount: {item.amount}</p>
+            <p>Status: {item.status}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Transaction;
