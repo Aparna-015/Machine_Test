bfa37897877eb4a99306956d23a221dfcf01ff1c
Aparna-015 <aparnaraman995@gmail.com>
2026-09-05 21:03:52 +0530

    added

--- transaction-app/src/component/Transaction.jsx
+++ transaction-app/src/component/Transaction.jsx
@@ -154,8 +154,7 @@ function UserProfile() {
     <div style={{ padding: "20px", border: "1px solid #ccc", borderRadius: "8px" }}>
        <h2>✅ User Details (Promise Fulfilled)</h2>
       <p><strong>Name:</strong> {user.name}</p>
-      <p><strong>Email:</strong> {user.email}</p>
-      <p><strong>City:</strong> {user.address.city}</p>
+     
        
       Button to re-trigger the Promise
       <button onClick={fetchUserData}>Reload User</button>
